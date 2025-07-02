8299279662e6fe2c7a21fcf2d62c5eb75296763b
choco-bear <kdy030529@gmail.com>
2025-07-02 19:05:50 +0900

    Removed some unnecessary lemmas

--- theories/lib/facts.v
+++ theories/lib/facts.v
@@ -1,24 +1,6 @@
 From stdpp Require Export relations.
 From stdpp Require Import binders gmap.
 
-Lemma if_iff P Q R S:
-  (P ↔ Q) →
-  (R ↔ S) →
-  ((P → R) ↔ (Q → S)).
-Proof.
-  naive_solver.
-Qed.
-
-Lemma if_iff' P R S :
-  (P → R ↔ S) → (P → R) ↔ (P → S).
-Proof. tauto. Qed.
-Lemma and_iff' (P R S : Prop) :
-  (P → R ↔ S) → (P ∧ R) ↔ (P ∧ S).
-Proof. tauto. Qed.
-Lemma and_iff (P Q R S : Prop) :
-  (P ↔ Q) → ((P ∨ Q) → R ↔ S) → (P ∧ R) ↔ (Q ∧ S).
-Proof. tauto. Qed.
-
 Lemma list_subseteq_cons {X} (A B : list X) x : A ⊆ B → x :: A ⊆ x :: B.
 Proof. intros Hincl. intros y. rewrite !elem_of_cons. naive_solver. Qed.
 Lemma list_subseteq_cons_binder A B x : A ⊆ B → x :b: A ⊆ x :b: B.
