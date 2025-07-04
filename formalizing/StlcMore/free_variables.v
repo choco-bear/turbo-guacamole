f57be0ea1e6b0b5b03bbfcbef488d3782a535876
choco-bear <kdy030529@gmail.com>
2025-07-05 07:26:59 +0900

    Added some lemmas about relations between free variables and substitution

--- formalizing/StlcMore/free_variables.v
+++ formalizing/StlcMore/free_variables.v
@@ -1,6 +1,6 @@
 Set Warnings "-notation-overridden,-parsing,-deprecated-hint-without-locality".
 From Formalizing.StlcMore Require Import notation tactics.
-From Intro2TT.lib Require Import maps.
+From Intro2TT Require Import Tactics lib.maps.
 
 (** ** Free Variables *)
 (** This file defines the notion of free variables in the StlcMore. *)
@@ -28,7 +28,7 @@ Proof. eauto. Qed.
 Goal FV (λ: "x", "x") = ∅.
 Proof. eauto. Qed.
 
-(** free variables and closedness *)
+(** Free variables and closedness *)
 Lemma is_closed_free_variables X e :
   is_closed (elements X) e → FV e ⊆ X.
 Proof.
@@ -63,4 +63,61 @@ Proof.
   - simpl; rewrite <-fin_sets.elements_union_singleton; eauto.
     apply IHe. intros y Hy. apply elem_of_union.
     destruct (decide (x = y)); [subst; left|right; apply H]; set_solver.
+Qed.
+
+(** Free variables and substitution *)
+Lemma free_variables_no_subst x es e :
+  x ∉ FV e → FV (subst x es e) = FV e.
+Proof.
+  revert x es; induction e as [|y|y e| | | | | | | | | |];
+    simpl; intros x es Hx; des_ifs;
+  repeat match goal with
+  | x : binder |- _ => destruct x
+  | H : context [{[BNamed ?s]}] |- _ =>
+      change {[BNamed s]} with ({[s]} : gset string) in H
+  | |- context [{[BNamed ?s]}] =>
+      change {[BNamed s]} with ({[s]} : gset string)
+  end; set_solver. (* SLOW: Around 3 secs *)
+Qed.
+
+Lemma free_variables_do_subst x es e :
+  FV es = ∅ → x ∈ FV e → FV (subst x es e) = FV e ∖ {[x]}.
+Proof.
+  revert x es; induction e as [|y|y e| | | | | | | | | |];
+    simpl; intros x es Hes Hy; des_ifs;
+  repeat match goal with
+  | x : binder |- _ => destruct x
+  | H : context [{[BNamed ?s]}] |- _ =>
+      change {[BNamed s]} with ({[s]} : gset string) in H
+  | |- context [{[BNamed ?s]}] =>
+      change {[BNamed s]} with ({[s]} : gset string)
+  end.
+  1-7,9,13-16: set_solver.
+  all: destruct (decide (x ∈ FV e1)); destruct (decide (x ∈ FV e2));
+    try destruct (decide (x ∈ FV e3)).
+  all: repeat match goal with
+  | H : ?x ∈ _ ∪ _ |- _ =>
+      apply elem_of_union in H; destruct H as [H|H]
+  | H : ?x ∈ ?X, H' : ?x ∉ ?X |- _ =>
+      exfalso; apply H', H
+  | H : ?x ∉ FV ?e |- _ =>
+      rewrite (free_variables_no_subst x _ e); eauto
+  | H : ?x ∈ FV ?e, IH : context[FV (subst _ _ ?e) = FV ?e ∖ {[_]}] |- context[FV (subst _ _ ?e)] =>
+      rewrite IH; eauto
+  | |- context[(_ ∪ _) ∖ _] =>
+      rewrite <-sets.union_difference
+  end.
+  4,8,17-20,24,33-36: rewrite (sets.not_elem_of_difference x (FV e1)); eauto.
+  3,9,14,17,23-25,28,34-36: rewrite (sets.not_elem_of_difference x (FV e2)); eauto.
+  9,13,18,22,28,29,35,36: rewrite (sets.not_elem_of_difference x (FV e3)); eauto.
+  all: by rewrite ?difference_shadow.
+Qed.
+
+Lemma free_variables_subst' x es e :
+  FV es = ∅ → {[x]} ⊆ FV e → FV (subst' x es e) = FV e ∖ {[x]}.
+Proof.
+  destruct x as [|x]; [simpl; set_solver|].
+  change {[_]} with ({[x]} : gset string).
+  intros Hes Hx%sets.singleton_subseteq_l.
+  by apply free_variables_do_subst.
 Qed.
