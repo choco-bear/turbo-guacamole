c4dd27605d2d910ffcaf6af7d3ba697e8c693a22
choco-bear <kdy030529@gmail.com>
2025-08-01 03:59:53 +0900

    Defined a lemma and proved preservation theorem using the lemma

--- formalizing/SystemF/soundness.v
+++ formalizing/SystemF/soundness.v
@@ -25,3 +25,22 @@ Proof.
   all: try match goal with x : bool |- _ => destruct x end;
        apply base_reducible_reducible; unfold base_reducible; simpl; eauto.
 Qed.
+
+
+(** Preservation Theorem *)
+Lemma typed_preservation_base_step e e' A :
+  TY 0; ∅ ⊢ e : A →
+  e ↝b e' →
+  TY 0; ∅ ⊢ e' : A.
+Proof. (* TODO *) Admitted.
+
+Theorem typed_preservation e e' A :
+  TY 0; ∅ ⊢ e : A →
+  e ↝ e' →
+  TY 0; ∅ ⊢ e' : A.
+Proof.
+  intros Hty Hstep. destruct Hstep as [K e1 e2 -> -> Hstep].
+  eapply fill_typing_decompose in Hty as (B & Hty & Hctx).
+  eapply fill_typing_compose; last done.
+  by eapply typed_preservation_base_step.
+Qed.
